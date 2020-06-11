0cdb1776df39222562d1dfa59e00cce76050d9ad
Katarina <katarinatina369@gmail.com>
2020-06-11 10:31:30 +0200

    find a movie

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,196 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="inputContainer">
+    <i class="fa fa-search"></i><input type="text" v-model = "searchTerm" v-on:keyup="updateList" v-bind:placeholder="placeholderValues.search">
+    <i class="fa fa-plus"></i>
+
+    
+       
+    <select name="list" id="list" v-model="searchType" v-on:click="updateList">
+    <option value="1">Movies</option>
+    <option value="2">Series</option>
+    <option value="3">Episodes</option>
+</select>
+ 
+    </div>
+
+
+  <ul v-if="list">
+
+    <li v-for="(item, index) in list" v-bind:key="index">
+      <img v-bind:src="item.Poster" alt="">
+
+       <h2> {{item.Title}}  ({{item.Year}}) </h2>
+       <p>{{item.Type}}</p>
+       <p>Movie</p>     
+    </li>
+    
+  </ul>
+  <p  v-if="error">{{ this.error}}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+ data(){
+   return{
+     placeholderValues:{
+       search:'Search...'
+     },
+
+     apiKey: "c81ffb3d",
+     searchTerm: '',
+     error:"",
+   list: null,
+   searchType: ""
+    
+   } 
+
+},
+
+methods: {
+    updateList: function(){
+      axios
+      .get('http://www.omdbapi.com/?s=' + this.searchTerm + '&apikey=c81ffb3d')
+      .then(response => {
+        if(response.data.Response == "True" ){
+        this.list = response.data.Search;
+        this.error = null;
+        console.log(response.data.Search);
+        } else {
+          if(response.data.Error == "Something went wrong.")
+          {
+            this.error = "Search for something."
+            
+          }else{
+            this.error = response.data.Error; 
+          }
+          this.list = null;
+
+        }
+        
+      })
+      .catch(this.error ="Something went wrong, please try again later.")
+      
+
+    }
+  } 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+
+.hello{
+  
+
+.inputContainer{
+  position: relative;
+     
+
+  input{
+    background-color: #1BBC9B;
+    width:800px;
+    height:70px;
+    border: none;
+    font-size: 20px;
+    border: 1px solid #1BBC9B; 
+    border-radius: 7px;
+    color:#34495E;
+    padding-left: 50px;
+
+
+  }
+
+.fa-search{
+  position:absolute;
+  top:27px;
+  left:250px;
+  color:#34495E;
+  font-size: 20px;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.fa-plus{
+  position: absolute;
+    color:#34495E;
+      font-size: 23px;
+      top:25px;
+      left:1030px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+select{
+  width:200px;
+  height:73px;
+  background-color:#34495E ;
+  border-radius: 7px; 
+  font-size:20px; 
+  cursor: pointer;
+
+
+  option{
+    font-size:20px;
+    background-color:#fff;
+    
+
+  }
+
 }
-a {
-  color: #42b983;
+
+  }
+
+  ul{
+     display:inline - block;
+     margin:0 auto;
+      margin-left: -40px;
+
+
+
+    li{
+      position: relative;
+      list-style-type: none;
+      display:flex;
+      justify-content: space-between;
+      align-items: center;
+      width:1025px;
+      text-align: center;
+      background-color:#34495E;
+      height: 70px;
+      padding:15px;
+      font-size:15px;
+      color:#fff;
+      // font-family: 'Arial', sans-serif;
+      position: relative;
+      
+      &:last-child{
+        border-radius: 0 0 7px 0;
+      }
+
+      img{
+        position:absolute;
+        top:0;
+        left:0;
+        width:80px;
+        height: 80px;
+        padding:10px;
+      }
+
+      p{
+        font-size: 17px;
+        text-align: right;
+       
+        text-transform: capitalize;
+      }
+
+
+    }
+
+       
+  }
 }
+
 </style>
